b954a473bebada99e192abc11dcb59e13ed68a20
Cix8 <francesco.natal_fn@libero.it>
2022-04-11 14:18:16 +0200

    add bg-color-program

--- js/script.js
+++ js/script.js
@@ -6,20 +6,26 @@ const containerWrapper = document.getElementById('general-container');
 
 for (let i = 1; i <= 100; i++) {
     let screen;
+    let colorClass;
+    const divElement = document.createElement('div');
+
     if (i % 3 == 0 && i % 5 != 0) {
         screen = multiplesOfTree;
+        colorClass = 'bg-multiples-of-tree';
     } else if (i % 5 == 0 && i % 3 != 0) {
         screen = multiplesOfFive;
+        colorClass = 'bg-multiples-of-five';
     } else if (i % 3 == 0 && i % 5 == 0) {
         screen = multiplesOfBoth;
+        colorClass = 'bg-multiples-of-both';
     } else {
         screen = i;
+        colorClass = 'bg-numbers';
     }
 
     console.log(screen);
     
-    const divElement = document.createElement('div');
-    divElement.classList.add('square');
+    divElement.classList.add('square', colorClass);
     divElement.innerHTML = screen;
 
     containerWrapper.append(divElement);
